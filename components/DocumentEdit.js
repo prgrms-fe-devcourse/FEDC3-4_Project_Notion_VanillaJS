69ce2359dc9e69a70c2add201f39034b0ad4f604
minjongbaek <minjongbaek@gmail.com>
2022-11-08 15:53:12 +0900

    Feat: Document의 제목과 내용을 그리는 DocumentEdit 컴포넌트 작성

---
+++ components/DocumentEdit.js
@@ -0,0 +1,21 @@
+export default function DocumentEdit({ $target, initialState }) {
+  this.$element = document.createElement('div');
+  this.$element.className = 'document-wrap'
+  $target.appendChild(this.$element);
+
+  this.state = initialState;
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  }
+
+  this.render = () => {
+    this.$element.innerHTML = `
+      <input class="document-title" type="text" name="title" value="${this.state.title}">
+      <textarea class="document-class" name="content">${this.state.content ? this.state.content : ''}</textarea>
+    `;
+  }
+
+  this.render();
+}
