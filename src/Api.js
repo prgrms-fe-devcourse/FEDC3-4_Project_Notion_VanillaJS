148266923d08dfe7433b9c8afab6377db0d9343b
JaeKwan Ahn <ggstork@gmail.com>
2022-11-09 01:24:41 +0900

    Fix : Api 호출 수정

--- src/Api.js
+++ src/Api.js
@@ -1,15 +1,18 @@
-const API_END_POINT = 'https://kdt-frontend.programmers.co.kr/'
+const API_END_POINT = "https://kdt-frontend.programmers.co.kr/";
 
-export const request = async ({ url, options }) => {
-    try {
-        console.log(url)
-        const res = await fetch(`${API_END_POINT}${url}`, options)
-        if (res.ok) {
-            const json = JSON.parse(res);
-            return json
-        }
-        throw new Error('fetch fail')
-    } catch (e) {
-        alert(e.message)
+export const request = async (url, options = {}) => {
+  try {
+    const res = await fetch(`${API_END_POINT}${url}`, {
+      ...options,
+      headers: {
+        "x-username": "ajk",
+      },
+    });
+    if (res.ok) {
+      return await res.json();
     }
-}
+    throw new Error("fetch fail");
+  } catch (e) {
+    alert(e.message);
+  }
+};
