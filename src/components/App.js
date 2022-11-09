c8c8c1dac5c62f39c06ba3fa9e9566d8d0468ac1
kmww <kmw9708@gmail.com>
2022-11-09 14:29:32 +0900

    Refactor: "컴포넌트 분리"

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,7 @@
-import DirectoryList from "./DirectoryList.js";
+import DocumentPage from "./DocumentPage.js";
 
 export default function App({ $target }) {
-  const DUMMY_DATA = [
-    {
-      id: 1, // Document id
-      title: "노션을 만들자", // Document title
-      documents: [
-        {
-          id: 2,
-          title: "블라블라",
-          documents: [
-            {
-              id: 3,
-              title: "함냐함냐",
-              documents: [],
-            },
-          ],
-        },
-      ],
-    },
-    {
-      id: 4,
-      title: "hello!",
-      documents: [
-        {
-          id: 5,
-          title: "살려줘",
-          documents: [],
-        },
-      ],
-    },
-  ];
-  new DirectoryList({
+  new DocumentPage({
     $target,
-    initialState: DUMMY_DATA,
   });
 }
--- src/components/DirectoryList.js
+++ src/components/DirectoryList.js
@@ -1,4 +1,5 @@
 import { setItem, getItem, removeItem } from "../utils/storage.js";
+import { totalDirectoryList } from "../utils/totalDirectoryList.js";
 
 export default function DirectoryList({ $target, initialState }) {
   const $directoryList = document.createElement("div");
@@ -9,37 +10,6 @@ export default function DirectoryList({ $target, initialState }) {
   this.setState = (nextState) => {
     this.state = nextState;
     this.render();
-  };
-
-  const totalDirectoryList = (directorys) => {
-    return `
-      <ul>
-        ${directorys
-          .map((directory) => {
-            const listToggleState = `isOpened-${directory.id}`;
-            const display = getItem(listToggleState) || "none";
-            return `
-              <li data-id="${directory.id}" name="list">
-              <span class="list-title"><button class="toggle-button" isClosed="true">></button>${
-                directory.title
-              }</span>
-              <span class="list-button"><button name="add-button" data-id="${
-                directory.id
-              }">+</button>
-              <button name="remove-button" data-id=${directory.id}>-</button></span>
-                ${
-                  directory.documents.length
-                    ? `<ul style="display: ${display}">${totalDirectoryList(
-                        directory.documents
-                      )}</ul>`
-                    : `<ul style="display: ${display}"><li>하위 페이지가 없습니다.</li></ul>`
-                }
-              </li>
-            `;
-          })
-          .join("")}
-      </ul>
-    `;
   };
 
   this.render = () => {
