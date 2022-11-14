05f68f14881d0254746d254f5dda376f0fe57d51
JooChunWook <jha970107@gmail.com>
2022-11-14 18:53:59 +0900

    Feat: API 관련 추가

---
+++ src/api/api.js
@@ -0,0 +1,19 @@
+import { API_END_POINT } from "../../config.js";
+
+export const request = async (url, options = {}) => {
+	try {
+		const res = await fetch(`${API_END_POINT}${url}`, {
+			...options,
+			headers: {
+				"Content-Type": "application/json",
+				"x-username": "woogie",
+			},
+		});
+		if (res.ok) {
+			return await res.json();
+		}
+		throw new Error("API 호출 실패!!!!!!!");
+	} catch (error) {
+		alert(error);
+	}
+};
