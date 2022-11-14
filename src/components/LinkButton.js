5ad629fe32dc446e42c152691e159d9793ddcf63
HyeJun <solar3070@naver.com>
2022-11-14 18:11:54 +0900

    feat: 버튼 컴포넌트 추가

---
+++ src/components/LinkButton.js
@@ -0,0 +1,12 @@
+export default function LinkButton({ $target, initialState }) {
+  this.state = initialState;
+
+  const $linkButton = document.createElement("button");
+  $target.appendChild($linkButton);
+
+  this.render = () => {
+    $linkButton.textContent = this.state.text;
+  };
+
+  this.render();
+}
