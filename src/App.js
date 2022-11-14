d1b8220857d565ba6f58b21efe6303e58c7456b0
HyeJun <solar3070@naver.com>
2022-11-14 22:04:31 +0900

    feat: 라우팅 처리

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import PostsPage from "./pages/PostsPage.js";
+import PostEditPage from "./pages/PostEditPage.js";
 
 export default function App({ $target }) {
   const postsPage = new PostsPage({ $target });
+  const postEditPage = new PostEditPage({
+    $target,
+    initialState: {
+      postId: "new",
+      post: {
+        title: "",
+        content: "",
+      },
+    },
+  });
 
-  postsPage.render();
+  this.route = () => {
+    const { pathname } = window.location;
+
+    if (pathname === "/") {
+      postsPage.render();
+    } else if (pathname.indexOf("/posts/") === 0) {
+      const [, , postId] = pathname.split("/");
+      postEditPage.setState({ postId });
+    }
+  };
+
+  this.route();
 }
--- src/main.js
+++ src/main.js
@@ -1,17 +1,5 @@
 import App from "./App.js";
-import PostEditPage from "./pages/PostEditPage.js";
 
 const $target = document.querySelector("#app");
 
-// new App({ $target });
-
-const postEditPage = new PostEditPage({
-  $target,
-  initialState: {
-    postId: "new",
-  },
-});
-
-postEditPage.setState({
-  postId: "53471",
-});
+new App({ $target });
