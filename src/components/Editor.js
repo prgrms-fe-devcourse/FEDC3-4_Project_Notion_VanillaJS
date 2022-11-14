47b7ffe0fc5b77983b7b922242a57d2c5723bd37
HyeJun <solar3070@naver.com>
2022-11-14 18:41:32 +0900

    feat: Editor 컴포넌트 생성

---
+++ src/components/Editor.js
@@ -0,0 +1,44 @@
+export default function Editor({
+  $target,
+  initialState = { title: "", content: "" },
+}) {
+  const $editor = document.createElement("div");
+  $target.appendChild($editor);
+
+  this.state = initialState;
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
+
+  let isInitialize = false;
+
+  this.render = () => {
+    if (!isInitialize) {
+      $editor.innerHTML = `
+      <input type="text" name="title" style="width: 600px;" value="${this.state.title}" />
+      <textarea name="content" style="width: 600px; height: 400px;">${this.state.content}</textarea>
+    `;
+
+      isInitialize = true;
+    }
+  };
+
+  this.render();
+
+  $editor.addEventListener("keyup", (e) => {
+    const { target } = e;
+
+    const name = target.getAttribute("name");
+
+    if (this.state[name]) {
+      const nextState = {
+        ...this.state,
+        [name]: target.value,
+      };
+
+      this.setState(nextState);
+    }
+  });
+}
--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import App from "./App.js";
+import Editor from "./components/Editor.js";
 
 const $target = document.querySelector("#app");
 
-new App({ $target });
+// new App({ $target });
+
+new Editor({
+  $target,
+  initialState: {
+    title: "노션 클로닝을 하자",
+    content: "아좌좌",
+  },
+});
