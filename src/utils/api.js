869c9530eda11a7b0f3109e6ad4ab956d6872091
HyeJun <solar3070@naver.com>
2022-11-14 17:35:34 +0900

    feat: api 요청 함수 작성

---
+++ src/utils/api.js
@@ -0,0 +1,21 @@
+import { API_END_POINT } from "../../baseUrl.js";
+
+export const request = async (url, options = {}) => {
+  try {
+    const res = await fetch(`${API_END_POINT}${url}`, {
+      ...options,
+      headers: {
+        "Content-type": "application/json",
+        "x-username": "roto",
+      },
+    });
+
+    if (res.ok) {
+      return await res.json();
+    }
+
+    throw new Error("API 처리 오류");
+  } catch (e) {
+    console.error(e);
+  }
+};
