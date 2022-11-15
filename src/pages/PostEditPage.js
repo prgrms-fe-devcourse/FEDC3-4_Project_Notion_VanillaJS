2d97090afbd225223d4a5913b3ea6b8eccb35581
HyeJun <solar3070@naver.com>
2022-11-15 23:23:35 +0900

    feat: 페이지 추가 버튼 클릭시 문서 바로 생성

--- src/pages/PostEditPage.js
+++ src/pages/PostEditPage.js
@@ -30,25 +30,11 @@ export default function PostEditPage({ $target, initialState }) {
           tempSaveDate: new Date(),
         });
 
-        const isNew = this.state.postId === "new";
-        if (isNew) {
-          const createdPost = await request("/documents", {
-            method: "POST",
-            body: JSON.stringify(post),
-          });
-          history.replaceState(null, null, `/posts/${createdPost.id}`);
-          removeItem(postLocalSaveKey);
-
-          this.setState({
-            postId: createdPost.id,
-          });
-        } else {
-          await request(`/documents/${post.id}`, {
-            method: "PUT",
-            body: JSON.stringify(post),
-          });
-          removeItem(postLocalSaveKey);
-        }
+        await request(`/documents/${post.id}`, {
+          method: "PUT",
+          body: JSON.stringify(post),
+        });
+        removeItem(postLocalSaveKey);
       }, 1000);
     },
   });
@@ -62,7 +48,6 @@ export default function PostEditPage({ $target, initialState }) {
         title: "",
         content: "",
       });
-
       const tempSaveDate = tempPost.tempSaveDate;
       if (tempSaveDate && tempSaveDate > post.updatedAt) {
         if (confirm("저장되지 않은 데이터가 있습니다. 불러올까요?")) {
@@ -73,7 +58,6 @@ export default function PostEditPage({ $target, initialState }) {
           return;
         }
       }
-
       this.setState({
         ...this.state,
         post,
@@ -81,28 +65,45 @@ export default function PostEditPage({ $target, initialState }) {
     }
   };
 
+  const createPost = async () => {
+    const createdPost = await request("/documents", {
+      method: "POST",
+      body: JSON.stringify({
+        title: "",
+        content: "",
+      }),
+    });
+    history.replaceState(null, null, `/posts/${createdPost.id}`);
+    this.setState({
+      postId: createdPost.id,
+    });
+  };
+
   this.setState = async (nextState) => {
+    if (nextState.postId === "new") {
+      await createPost();
+      return;
+    }
+
     if (this.state.postId !== nextState.postId) {
       postLocalSaveKey = `temp-post-${this.state.postId}`;
       this.state = nextState;
 
       if (this.state.postId === "new") {
+        console.log("new?");
         const post = getItem(postLocalSaveKey, {
           title: "",
           content: "",
         });
         this.render();
         editor.setState(post);
-      } else {
-        await fetchPost();
       }
-
+      await fetchPost();
       return;
     }
 
     this.state = nextState;
     this.render();
-
     editor.setState(
       this.state.post || {
         title: "",
