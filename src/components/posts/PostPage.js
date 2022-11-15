07d8abf63a1e1ea3ab1712d95621b52011aacb40
parkmoen <pmj6516@naver.com>
2022-11-15 17:36:06 +0900

    Remove(PostPage): 사용하지 않는 Post Page 자식 컴포넌트 삭제하기
    ## Summary
    - PostTitle 컴포넌트 삭제하기
    - PostContent 컴포넌트 삭제하기

--- src/components/posts/PostPage.js
+++ src/components/posts/PostPage.js
@@ -1,15 +1,10 @@
 import createElementHelper from '../../utils/helpers.js';
 
-import PostTitle from './PostTitle.js';
-import PostContent from './PostContent.js';
-
 class PostPage {
   constructor({ $target }) {
     this.$target = $target;
     this.$page = createElementHelper('div', '.post-container');
 
-    new PostTitle({ $target: this.$page });
-    new PostContent({ $target: this.$page });
     this.render();
   }
 
