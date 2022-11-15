8a73357a2a2f8178e205a2fc218f85202afcf3d2
JooChunWook <jha970107@gmail.com>
2022-11-15 17:26:45 +0900

    Fix: 포스트 리스트 스타일링 수정

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,4 @@
 import { $home } from "../utils/templates.js";
-import PostEdit from "../components/PostEdit.js";
-import PostsPage from "../components/PostsPage.js";
-import { request } from "../api/api.js";
-import { initRouter } from "../routes/router.js";
 
 export default function HomePage({ $target }) {
 	const $homePage = document.createElement("div");
@@ -14,29 +10,4 @@ export default function HomePage({ $target }) {
 	};
 
 	this.render();
-
-	// this.init = () => {
-	// 	const { pathname } = window.location;
-	// 	if (pathname === "/") {
-	// 		postsPage.setState();
-	// 		// $postEditContainer.innerHTML = $home();
-	// 	} else if (pathname.indexOf("/documents/") === 0) {
-	// 		const [, , id] = pathname.split("/");
-	// 		if (id === "new") {
-	// 			postEdit.setState({
-	// 				...this.state,
-	// 				id,
-	// 			});
-	// 			postsPage.setState();
-	// 		} else {
-	// 			postsPage.setState();
-	// 		}
-	// 	}
-	// };
-	// this.init();
-	// initRouter(() => this.init());
-
-	// window.addEventListener("popstate", () => {
-	// 	this.init();
-	// });
 }
