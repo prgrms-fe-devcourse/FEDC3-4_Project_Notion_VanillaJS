95bbce3344661a0b0ae963fcb1de994569bb03d4
guno517 <guno5170@gmail.com>
2022-11-15 14:31:23 +0900

    Feat: 하위 document 링크로 이동

--- src/components/ChildDocument.js
+++ src/components/ChildDocument.js
@@ -1,3 +1,5 @@
+import { push } from "../router.js";
+
 export default function ChildDocument({ $target, initialState }) {
   const $childDocument = document.createElement("div");
   $childDocument.className = "child-document";
@@ -8,29 +10,30 @@ export default function ChildDocument({ $target, initialState }) {
 
   this.setState = (nextState) => {
     this.state = nextState;
-    console.log(this.state);
     this.render();
   };
 
   this.render = () => {
     $childDocument.innerHTML = `
       ${this.state.documents
-        .map(
-          (document) =>
-            `${
+        .map((document) => {
+          return `
+            ${
               document
-                ? `<div class="child-link">${document.title}</div>`
+                ? `<div data-id="${document.id}" class="child-link">${document.title}</div>`
                 : `<div>하위 페이지가 없음</div>`
-            }`
-        )
+            }
+            `;
+        })
         .join("")}
     `;
   };
 
   $childDocument.addEventListener("click", (e) => {
     const $childList = e.target.closest(".child-link");
+    const { id } = $childList.dataset;
     if ($childList) {
-      console.log(this.state.documents);
+      push(`/documents/${id}`);
     }
   });
 }
