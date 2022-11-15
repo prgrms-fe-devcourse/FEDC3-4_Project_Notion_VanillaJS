105c534b516c1a8d0345793a87304cf46b29ef33
parkmoen <pmj6516@naver.com>
2022-11-16 02:49:56 +0900

    Comment(Utils): createElementHelper helper 함수 예외처리 메모하기

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,9 @@
 const createElementHelper = (tagName, ...options) => {
+  /* 예외처리 List
+   - options(atrributeName)의 첫번째 자리가 # or .가 아닌 경우 예외처리하기 
+   - tagName의 type이 string이 아니거나 빈값인 경우 예외처리하기
+   */
+
   const $element = document.createElement(tagName);
 
   if (options.length) {
