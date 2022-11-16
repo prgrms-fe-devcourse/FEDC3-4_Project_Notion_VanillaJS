0d3c669416483422041cff10fddbe1b1fae36343
EUNA <beni1026@naver.com>
2022-11-16 19:51:38 +0900

    feat: Header component

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import { push } from "./router.js";
+
+export default function Header({ $target, $name }) {
+  const $header = document.createElement("header");
+  $header.style.cursor = "pointer";
+
+  $target.appendChild($header);
+
+  this.render = () => {
+    $header.innerHTML = `
+      📌&nbsp<span class="title">${$name}</span>&nbsp의 Notion
+    `;
+  };
+
+  $header.addEventListener("click", (e) => {
+    push("/");
+  });
+
+  this.render();
+}
