f3575aae639c99403a5fe019053c028126c8b2df
Sunghum Paik <zerosial@gmail.com>
2022-11-16 17:38:37 +0900

    feat: env 테스트

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import App from './App.js';
+import * as dotenv from 'dotenv';
+dotenv.config();
+console.log(process.env.API_END_POINT);
 
 const $app = document.querySelector('.app');
 $app.className = 'h-screen grid grid-cols-5';
