58880eaf14d9ea4ae3b6f29f8722b4a6e08baf96
mxx-kor <minjae7728@gmail.com>
2022-11-16 22:06:02 +0900

    Feat: "추가 LandingPage"
    초기나 오류 발생시 화면 처리를 위한 랜딩페이지를 구현

---
+++ src/components/editpage/LandingPage.js
@@ -0,0 +1,20 @@
+import { validation } from "../../validation.js";
+
+export default function LandingPage({ $target }) {
+  validation(new.target, "LandingPage");
+
+  const $landingPage = document.createElement("section");
+  $landingPage.className = "landingPage";
+  $target.appendChild($landingPage);
+
+  this.render = () => {
+    $landingPage.innerHTML = `
+            <h1 class="landingTitle">
+                Notion에 오신 것을 환영합니다.
+            </h1>
+            <p class="landingP">페이지를 추가하여 새 문서를 작성해보세요.</p>
+        `;
+  };
+
+  this.render();
+}
