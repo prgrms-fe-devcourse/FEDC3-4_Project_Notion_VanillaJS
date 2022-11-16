3a82569b7708ae785a6c5744569ccddc66925b54
Sunghum Paik <zerosial@gmail.com>
2022-11-16 18:20:49 +0900

    fix: process.env 수정

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import App from './App.js';
 
-const samp = process.env.API_END_POINT;
-console.log(samp);
-
 const $app = document.querySelector('.app');
 $app.className = 'h-screen grid grid-cols-5';
 
