99a30c183646a5c08515bab377951b49ca113641
minjongbaek <minjongbaek@gmail.com>
2022-11-17 03:13:55 +0900

    fix: 다른 documents의 subDocument가 보이는 이슈

--- components/SubDocumentsLink.js
+++ components/SubDocumentsLink.js
@@ -13,14 +13,16 @@ export default function SubDocumentsLink({ $target, initialState, onLinkClick })
 
   this.render = () => {
     const { id, documents } = this.state;
-    if (id === null || documents.length === 0) return;
-
-    this.$element.innerHTML = `
-      <h1>Sub Documents</h1>
-      ${documents.map(({ id, title }) => `
-        <div><a data-id="${id}" href="/documents/${id}" rel="noopener noreferrer">${title.trim() === '' ? 'Untitled' : title}</a></div>
-      `).join('')}
-    `;
+    if (id === null || documents.length === 0) {
+      this.$element.innerHTML = '';
+    } else {
+      this.$element.innerHTML = `
+        <h1>Sub Documents</h1>
+        ${documents.map(({ id, title }) => `
+          <div><a data-id="${id}" href="/documents/${id}" rel="noopener noreferrer">${title.trim() === '' ? 'Untitled' : title}</a></div>
+        `).join('')}
+      `;
+    }
   };
 
   this.$element.addEventListener('click', (e) => {
