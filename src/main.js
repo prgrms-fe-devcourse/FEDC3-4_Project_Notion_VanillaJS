a8c7dd927fcdfc295d102e6e0496f4c93ed6ca09
Sunghum Paik <zerosial@gmail.com>
2022-11-16 18:18:50 +0900

    fix: test

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import App from './App.js';
 
-console.log(process.env.API_END_POINT);
-console.log(process.env);
+const samp = process.env.API_END_POINT;
+console.log(samp);
 
 const $app = document.querySelector('.app');
 $app.className = 'h-screen grid grid-cols-5';
