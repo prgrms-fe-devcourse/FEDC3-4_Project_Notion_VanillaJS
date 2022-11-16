915695f57f05207fd2a5e48294fb09e5131ab1bf
Park-min-hyoung <qkralswgud1324@naver.com>
2022-11-16 15:52:05 +0900

    ✨ Feat: 추가 fold 기능

--- src/components/common/DocumentList.js
+++ src/components/common/DocumentList.js
@@ -1,4 +1,5 @@
 import { push } from "../../router/router.js";
+import { changeFold } from "../../js/folder.js";
 
 export default function DocumentList({
   $documentListPage,
@@ -23,7 +24,12 @@ export default function DocumentList({
     this.render();
   };
 
+  let foldData;
+  let selectIndex;
   const listMarkUp = (list, width) => {
+    selectIndex += 1;
+    foldData.push({ width });
+
     let title;
     if (this.listState.updateEdit && this.listState.updateEdit.id === list.id) {
       title = this.listState.updateEdit.title;
@@ -40,7 +46,7 @@ export default function DocumentList({
 
     return `<div class="${containerClassName}" data-id="${list.id}">
       <li class="list" style="margin-left: ${width}px;">
-        <i class="fa-solid fa-chevron-right" data-id="${list.id}"></i>
+        <i class="fa-solid fa-chevron-down" data-id="${list.id}" data-index-id="${selectIndex}"></i>
         <i class="fa-brands fa-pagelines fa-lg"></i>
         <span class="list-container__lists--txt">${title}<span>
       </li>
@@ -76,6 +82,8 @@ export default function DocumentList({
   };
 
   this.render = () => {
+    foldData = [0];
+    selectIndex = 0;
     if (Array.isArray(this.listState.originEdit)) {
       $listPage.innerHTML = `
         <a href="/index.html" class="home">
@@ -99,9 +107,16 @@ export default function DocumentList({
     const $div = e.target.closest("div");
 
     if ($i) {
-      const { id } = $i.dataset;
-      const className = $i.classList.item(2);
-      if (className === "plus") {
+      const { id, indexId } = $i.dataset;
+      const className = $i.classList.item(2) || $i.getAttribute("class");
+
+      if (className === "fa-solid fa-chevron-down") {
+        $i.setAttribute("class", "fa-solid fa-chevron-right");
+        changeFold({ foldData, indexId, status: "none" });
+      } else if (className === "fa-solid fa-chevron-right") {
+        $i.setAttribute("class", "fa-solid fa-chevron-down");
+        changeFold({ foldData, indexId, status: "flex" });
+      } else if (className === "plus") {
         onPlus(id);
       } else if (className === "remove") {
         onRemove(id);
