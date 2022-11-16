2737c23948c069b80e094f2622c8681389f536f4
Seung-hwan285 <ghks123285@gmail.com>
2022-11-17 00:50:17 +0900

    feat : Navbar documentList , Header title 구현

---
+++ src/Navbar/DocumentList.js
@@ -0,0 +1,70 @@
+import { createElement, targetClosest, targetContains } from '../utils/dom.js';
+import { isNew } from '../utils/errorHandler.js';
+import { route } from '../utils/route.js';
+
+function DocumentList({ target, initialState, onDelete, onAdd }) {
+  isNew(new.target);
+  const postList = createElement('div');
+
+  target.appendChild(postList);
+
+  this.state = initialState;
+
+  this.setState = nextState => {
+    this.state = nextState;
+    this.render();
+  };
+
+  this.render = () => {
+    postList.innerHTML = paintDocument(this.state);
+    onClickDocument();
+  };
+
+  const paintDocument = docuements => {
+    return `
+        <ul class="document-list">
+        
+        ${docuements
+          .map(
+            post => `
+          <li class="list-style-type" id="${post.id}">
+          
+            <div class="row-item">
+                    
+                <div class="item-text">
+                    ${post.title ? post.title : '제목 없음'}
+                </div>
+                
+                <div class="item-button">
+                        <button class="delete-btn">X</button>
+                        <button class="add-btn">+</button>
+                </div>
+            </div>
+            ${post.documents.length ? paintDocument(post.documents) : ''}
+          </li>
+        `,
+          )
+          .join('')}
+      </ul>
+        `;
+  };
+
+  const onClickDocument = () => {
+    const ul = postList.querySelector('.document-list');
+
+    ul.addEventListener('click', e => {
+      const postId = targetClosest(e, 'li').id;
+
+      if (targetContains(e, 'delete-btn')) {
+        onDelete(postId);
+      } else if (targetContains(e, 'add-btn')) {
+        onAdd(postId);
+      } else {
+        route(`/documents/${postId}`);
+      }
+    });
+  };
+  this.render();
+}
+
+export default DocumentList;
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -3,6 +3,9 @@ import { createElement } from '../utils/dom.js';
 import { request } from '../utils/api.js';
 import { documentsUrl } from '../utils/util.js';
 import DocumentHeader from './DocumentHeader.js';
+import DocumentList from './DocumentList.js';
+import { addMethod, deleteMethod } from '../utils/optionsMethod.js';
+import { route } from '../utils/route.js';
 
 function Navbar({ target }) {
   isNew(new.target);
@@ -19,8 +22,58 @@ function Navbar({ target }) {
     },
   });
 
+  const documentList = new DocumentList({
+    target: nav,
+    initialState: [],
+    onDelete: async id => {
+      // 현재 Document 삭제하게 되면 하위 Documents 전부 삭제
+      const findCurrentId = () => {
+        const rootDocument = [...documentList.state];
+
+        while (rootDocument.length > 0) {
+          const cur = rootDocument.shift();
+
+          if (parseInt(cur.id) === parseInt(id)) {
+            console.log(cur);
+            return cur;
+          }
+
+          cur.documents.forEach(el => {
+            rootDocument.push(el);
+          });
+        }
+      };
+      const handlerDelete = async () => {
+        const currentDocument = findCurrentId();
+        const que = [currentDocument];
+        const queId = [currentDocument.id];
+        const current = que.shift();
+
+        while (que.length > 0) {
+          current.documents.forEach(el => {
+            que.push(el);
+            queId.push(el.id);
+          });
+        }
+
+        for (let i of queId) {
+          await deleteMethod(i);
+        }
+      };
+
+      await handlerDelete();
+      route('/');
+    },
+
+    onAdd: async id => {
+      const newPost = await addMethod(id);
+      route(`${documentsUrl}/${newPost.id}`);
+    },
+  });
+
   this.setState = async () => {
     const posts = await request(`${documentsUrl}`);
+    documentList.setState(posts);
     this.render();
   };
 
