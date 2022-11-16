1cdcf1b86e7d1da5a102cbab2b92b887c643efd3
Sunghum Paik <zerosial@gmail.com>
2022-11-16 17:40:50 +0900

    fix: action test

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import App from './App.js';
-import * as dotenv from 'dotenv';
-dotenv.config();
+
 console.log(process.env.API_END_POINT);
+console.log(process.env);
 
 const $app = document.querySelector('.app');
 $app.className = 'h-screen grid grid-cols-5';
