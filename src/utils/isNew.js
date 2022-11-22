796cf91eca56fdccf57fddf126b5bbdab99236ba
kmww <kmw9708@gmail.com>
2022-11-22 19:27:50 +0900

    Refactor: "isNew함수 변경"

--- src/utils/isNew.js
+++ src/utils/isNew.js
@@ -1,6 +1,5 @@
 export const isNew = (component, cur) => {
   if (cur instanceof component === false) {
-    console.log(`${component.name}는 생성자 함수 입니다. new를 사용하세요`);
-    return;
+    throw new Error(`${component.name}는 생성자 함수 입니다. new를 사용하세요`);
   }
 };
