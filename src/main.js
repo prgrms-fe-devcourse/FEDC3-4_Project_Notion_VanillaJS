4d5e22c595ba59d5283fba03e187a21f17788fcc
jynam <njy1007@gmail.com>
2022-11-24 09:36:55 +0900

    feat: onEdit에 debounce 적용

--- src/main.js
+++ src/main.js
@@ -8,13 +8,19 @@ const $app = document.querySelector('#app');
 
 const TEMP_POST_SAVE_KEY = 'temp-post';
 const post = getItem(TEMP_POST_SAVE_KEY, { title: '', content: '' });
+let timer = null;
 new Editor({
   $target: $app,
   props: {
     ...post,
     onEdit: (post) => {
-      // TODO 추후에 id 값 넣어서 저장해줘야 할지
-      setItem(TEMP_POST_SAVE_KEY, { ...post, tempSaveDate: new Date() });
+      if (timer !== null) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(() => {
+        // TODO 추후에 id 값 넣어서 저장해줘야 할지
+        setItem(TEMP_POST_SAVE_KEY, { ...post, tempSaveDate: new Date() });
+      }, 1000);
     },
   },
 });
