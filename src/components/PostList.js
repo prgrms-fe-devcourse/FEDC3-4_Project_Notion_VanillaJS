8246768ebba12bb403a324dadbcb5b624bdbb651
EUNA <beni1026@naver.com>
2022-11-27 15:14:11 +0900

    fix: postList 랜더링 함수 따로 뺌

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,3 +1,4 @@
+import { renderPost } from "../util/renderPost.js";
 import { push } from "../util/router.js";
 
 export default function PostList({ $target, initialState, onAdd, onDelete }) {
@@ -12,40 +13,10 @@ export default function PostList({ $target, initialState, onAdd, onDelete }) {
     this.render();
   };
 
-  const postListHandler = (lists) => {
-    return lists
-      .map(
-        (list) => `
-            <li data-id=${list.id} class="parentList">
-              <div class="content">
-                <div class="left-box">
-                  <img src="/src/img/arrow_right_icon.svg" class="toggleBtn"/>
-                  <div class="title" >${list.title}</div>
-                </div>
-                <div class="right-box">
-                  <img src="/src/img/minus_icon.svg" class="del" />
-                  <img src="/src/img/plus_icon.svg" class="add" />
-                </div>  
-              </div>
-              ${
-                list.documents.length !== 0
-                  ? `
-              <ul class="childList">
-                ${postListHandler(list.documents)}
-              </ul>   
-              `
-                  : ""
-              }              
-            </li>
-          `
-      )
-      .join("");
-  };
-
   this.render = () => {
     $postList.innerHTML = `
       <ul>
-        ${postListHandler(this.state)}
+        ${renderPost(this.state)}
       </ul>      
     `;
   };
@@ -53,10 +24,6 @@ export default function PostList({ $target, initialState, onAdd, onDelete }) {
   $postList.addEventListener("click", (e) => {
     const { className } = e.target;
     const $li = e.target.closest("li");
-    // const $title = e.target.closest(".title");
-    // const $toggle = e.target.closest(".toggleBtn");
-    // const $add = e.target.closest(".add");
-    // const $del = e.target.closest(".del");
 
     if (!$li) return;
     const { id } = $li.dataset;
@@ -64,6 +31,7 @@ export default function PostList({ $target, initialState, onAdd, onDelete }) {
     switch (className) {
       case "toggleBtn":
         const child = $li.childNodes[3]; // 하위 리스트
+        console.log(child);
         if (child) {
           if (child.style.display === "none") {
             child.style.display = "block";
