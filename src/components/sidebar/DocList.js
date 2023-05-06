378f80e2c647f33c5520bd4a36d38e6dabccb7cc
mxx-kor <minjae7728@gmail.com>
2023-05-06 18:33:09 +0900

    refactor: 열림, 닫힘 상태 저장 기능 추가

--- src/components/sidebar/DocList.js
+++ src/components/sidebar/DocList.js
@@ -1,10 +1,17 @@
-import { validation } from "../../validation.js";
+import { validation } from '../../validation.js';
+import { getItem, setItem } from '../../storage.js';
 
-export default function DocList({ $target, initialState, onClick, onNewSubDoc, onDelete }) {
-  validation(new.target, "DocList");
+export default function DocList({
+  $target,
+  initialState,
+  onClick,
+  onNewSubDoc,
+  onDelete,
+}) {
+  validation(new.target, 'DocList');
 
-  const $docList = document.createElement("nav");
-  $docList.className = "docList";
+  const $docList = document.createElement('nav');
+  $docList.className = 'docList';
   $target.appendChild($docList);
 
   this.state = initialState;
@@ -15,26 +22,33 @@ export default function DocList({ $target, initialState, onClick, onNewSubDoc, o
   };
 
   const renderDocList = (docs) => {
+    const openedItem = getItem('opened', []);
     return docs
       .map(
-        ({ id, title, documents }, i) => `
+        ({ id, title, documents }) => `
         <li id=${id} data-id=${id} class="docItem">
           <p class="forHover">
-            <button class="toggleFold btn">►</button>
+            <button class="toggleFold btn">${
+              openedItem.includes(id.toString()) ? '▼' : '►'
+            }</button>
             <span class="docTitle">${title}</span>
             <span class="controlBtns">
               <button class="newSubDoc btn">➕</button> 
               <button class="delete btn">X</button>
             </span>
             ${
-              documents.length > 0
+              openedItem.includes(id.toString())
+                ? documents.length > 0
+                  ? `<ul class='child--show'>${renderDocList(documents)}</ul>`
+                  : `<ul class='child--show'><li class="isEnd">하위 페이지가 없습니다.</li></ul>`
+                : documents.length > 0
                 ? `<ul class='child'>${renderDocList(documents)}</ul>`
                 : `<ul class='child'><li class="isEnd">하위 페이지가 없습니다.</li></ul>`
             }
           </p>
         </li>`
       )
-      .join("");
+      .join('');
   };
 
   this.render = () => {
@@ -47,37 +61,51 @@ export default function DocList({ $target, initialState, onClick, onNewSubDoc, o
 
   this.render();
 
-  $docList.addEventListener("click", (e) => {
-    const $li = e.target.closest("li");
+  $docList.addEventListener('click', (e) => {
+    const $li = e.target.closest('li');
 
     if (!$li) return;
     const { id } = $li.dataset;
     const { classList } = e.target;
 
-    if (classList.contains("toggleFold")) {
-      const $childUl = $li.getElementsByTagName("ul");
+    if (classList.contains('toggleFold')) {
+      const $childUl = $li.getElementsByTagName('ul');
       if ($childUl) {
-        const toggleDisplay = $childUl[0].classList.contains("child");
-        $childUl[0].className = toggleDisplay ? "child--show" : "child";
-        $li.querySelector(".toggleFold").innerText = toggleDisplay ? "▼" : "►";
+        const toggleDisplay = $childUl[0].classList.contains('child');
+        const prev = getItem('opened', []);
+        if (toggleDisplay) {
+          setItem('opened', [...prev, id]);
+        } else {
+          setItem(
+            'opened',
+            prev.filter((el) => el !== id)
+          );
+        }
+        $childUl[0].className = toggleDisplay ? 'child--show' : 'child';
+        $li.querySelector('.toggleFold').innerText = toggleDisplay ? '▼' : '►';
       }
       return;
     }
-    if (classList.contains("newSubDoc")) {
-      const $childUl = $li.getElementsByTagName("ul");
-      const $isEnd = $childUl[0].querySelector(".isEnd");
-      const $newSubDoc = document.createElement("li");
+    if (classList.contains('newSubDoc')) {
+      const $childUl = $li.getElementsByTagName('ul');
+      const $isEnd = $childUl[0].querySelector('.isEnd');
+      const $newSubDoc = document.createElement('li');
+
+      const prev = getItem('opened', []);
+      if (!prev.includes(id)) {
+        setItem('opened', [...prev, id]);
+      }
 
       if ($childUl.length > 0) {
-        $childUl[0].className = "child--show";
-        $li.querySelector(".toggleFold").innerText = "▼";
+        $childUl[0].className = 'child--show';
+        $li.querySelector('.toggleFold').innerText = '▼';
       }
 
       if ($isEnd) {
         $isEnd.remove();
       }
-      $newSubDoc.id = "newSubDoc";
-      $newSubDoc.className = "docItem";
+      $newSubDoc.id = 'newSubDoc';
+      $newSubDoc.className = 'docItem';
 
       $childUl[0].appendChild($newSubDoc);
       $newSubDoc.innerHTML = `
@@ -94,8 +122,8 @@ export default function DocList({ $target, initialState, onClick, onNewSubDoc, o
       onNewSubDoc(id);
       return;
     }
-    if (classList.contains("delete")) {
-      if (confirm("이 문서를 삭제하시겠습니까?")) {
+    if (classList.contains('delete')) {
+      if (confirm('이 문서를 삭제하시겠습니까?')) {
         $li.remove();
         onDelete(id);
       }
